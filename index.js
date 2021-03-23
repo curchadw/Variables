fbf035de409427f249d389a5af1b5b47d7bda844
curchadw <curtisdeveloper@gmail.com>
2021-03-22 20:33:21 -0500

    Added class exercise

--- index.js
+++ index.js
@@ -25,3 +25,36 @@ console.log(addition)
 
 const add = document.getElementById('add')
 add.innerHTML = `<p>${addition}</p>`
+
+
+//numbers
+let num = 2
+
+//should equal 4
+num += 2
+
+console.log(num)
+
+//should equal 4 squared
+
+console.log(num*num)
+
+let name2 = "Curtis";
+
+let studentId = 4;
+
+let myFunction = function(name2, studentId) {
+  return `name: ${name2}, studentID: " + ${studentId}`;
+};
+
+console.log(name2); 		// What is the output?
+
+console.log(studentId); 	// What is the output?
+
+console.log(myFunction(name2, studentId)); 	// What is the output?
+
+console.log(myFunction("Jeff", 7)); 	// What is the output?
+
+
+
+
