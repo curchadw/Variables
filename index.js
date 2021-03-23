49c24d7fe21dc0879cfd8a622c47f2ef4978c9ef
curchadw <curtisdeveloper@gmail.com>
2021-03-22 19:48:41 -0500

    Expanding on variables

--- index.js
+++ index.js
@@ -4,3 +4,24 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>Variables</h1>`;
+
+let name = 'Curtis'
+
+let language = 'JavaScript'
+
+console.log(name)
+console.log(language)
+
+const nameDiv = document.getElementById('name')
+nameDiv.innerHTML = `<p>${name}</p>`
+
+const langDiv = document.getElementById('language')
+langDiv.innerHTML = `<p>${language}</p>`
+
+
+let addition = 10 + 10
+
+console.log(addition)
+
+const add = document.getElementById('add')
+add.innerHTML = `<p>${addition}</p>`
